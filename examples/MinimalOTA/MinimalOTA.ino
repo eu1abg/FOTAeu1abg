e79d31ad98225b24c6cb0324ba4b7232d8d81643
Aleaksandr <124681749+eu1abg@users.noreply.github.com>
2025-12-26 17:40:02 +0300

    Add files via upload

--- examples/MinimalOTA/MinimalOTA.ino
+++ examples/MinimalOTA/MinimalOTA.ino
@@ -1,1 +1,16 @@
+#include <FOTAeu1abg.h>
 
+FOTAeu1abg fota("esp32-fota-http", 1, "your_SSID", "your_PASSWORD");
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  
+  fota.setManifestURL("https://raw.githubusercontent.com/eu1abg/Webasto_virtuino/main/firmware/firmware.json");
+  fota.begin();
+}
+
+void loop() {
+  fota.handle(); // Фоновая проверка обновлений
+  delay(100);
+}
